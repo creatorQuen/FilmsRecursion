54bdf5066540b492f3ce2dff8e3896a6d00138b1
creatorQuen <spastime@yandex.ru>
2021-05-07 12:48:39 +0300

    Add project files.

---
+++ FilmsRecursion/ZadachaZapolnenia.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FilmsRecursion
+{
+    public class ZadachaZapolnenia
+    {
+        public static List<int> BoxSizes; // Один общий для всех.
+        public int EmptyPlace; // Сколько места останлось в конкретной ноде.
+
+        public List<int> CurrentBoxes;
+
+        public ZadachaZapolnenia(int emptyPlace, List<int> currtneBox)
+        {
+            // Надо понимать сколько свободного места.
+            // И какие коробки уже лежат.
+
+            EmptyPlace = emptyPlace;
+            CurrentBoxes = currtneBox;
+
+        }
+
+    }
+}
